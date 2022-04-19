da95477c00cb1cb25c8d8a30efc22d9dcb66dc54
MarkEmdin <emdinmark@gmail.com>
2022-04-19 21:40:36 +0300

    add server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const useSocket = require('socket.io');
+
+const app = express();
+const server = require('http').Server(app);
+const io = useSocket(server);
+
+const rooms = new Map();
+
+app.get('/rooms', (req, res) => {
+  res.json(rooms);
+});
+
+server.listen(9999, (err) => {
+  if (err) {
+    throw Error(err);
+  }
+  console.log('server started');
+});
